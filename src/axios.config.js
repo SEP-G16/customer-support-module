c189325a6c8a9c0bb1d0b1e9b9066cd3cccd0474
Devindie Samaranayake <gdcsamaranayake2001@gmail.com>
2024-10-11 00:18:22 +0530

    axios.confi changed

--- src/axios.config.js
+++ src/axios.config.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
-let baseUrl = process.env.REACT_APP_BASE_URL 
+let baseUrl = process.env.REACT_APP_BASE_URL;
+
+// Fallback to HTTPS if in production mode
+if (process.env.NODE_ENV === 'production') {
+  baseUrl = baseUrl.replace('http://', 'https://');
+}
 
 export const AxiosInstance = axios.create({
-  baseURL: baseUrl,
+  baseURL: baseUrl,
 });
