1abd802fb0a166d26228e3f6c3bc1f22c9f26e03
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-14 20:31:50 +0530

    changes for deployemnt

--- src/axios.config.js
+++ src/axios.config.js
@@ -7,6 +7,10 @@ if (process.env.NODE_ENV === 'production') {
   baseUrl = baseUrl.replace('http://', 'https://');
 }
 
+if(process.env.REACT_APP_RUNTIME_MODE === 'DEPLOYMENT'){
+  baseUrl = null;
+}
+
 export const AxiosInstance = axios.create({
   baseURL: baseUrl,
 });
