80d1472a5701f1359544d0955893c08310b48cb4
VenuraKS25 <91336404+VenuraKS25@users.noreply.github.com>
2024-10-15 11:01:00 +0530

    baseurl set to null

--- src/axios.config.js
+++ src/axios.config.js
@@ -1,20 +1,5 @@
 import axios from "axios";
 
-let baseUrl = process.env.REACT_APP_BASE_URL;
-
-// Fallback to HTTPS if in production mode
-if (process.env.NODE_ENV === 'production') {
-  baseUrl = baseUrl.replace('http://', 'https://');
-}
-
-if(process.env.REACT_APP_RUNTIME_MODE === 'DEPLOYMENT'){
-  baseUrl = null;
-}
-
-console.log(baseUrl);
-
-console.log(process.env.REACT_APP_RUNTIME_MODE);
-
 export const AxiosInstance = axios.create({
-  baseURL: baseUrl,
+  baseURL: null,
 });
